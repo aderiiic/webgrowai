cf6f124b51a4f4fd43a62bec4026fd245a4b2487
amarderic <amar.deric@webbi.se>
2025-08-17 21:25:27 +0200

    test linkedin

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -102,6 +102,7 @@ class SocialAuthController extends Controller
     {
         Log::info('[LinkedIn] Redirecting');
         $scopes = config('services.linkedin.scopes', []);
+        Log::info('[LinkedIn] Scopes', ['scopes' => $scopes]);
         $params = [
             'response_type' => 'code',
             'client_id'     => config('services.linkedin.client_id'),
@@ -109,7 +110,9 @@ class SocialAuthController extends Controller
             'state'         => csrf_token(),
             'scope'         => implode(' ', $scopes),
         ];
+        Log::info('[LinkedIn] Params', ['params' => $params]);
         $url = 'https://www.linkedin.com/oauth/v2/authorization?' . http_build_query($params);
+        Log::info('[LinkedIn] URL', ['url' => $url]);
         return redirect()->away($url);
     }
 
