a2184db39990a105eeee4e4dbc4cb7f5132faf32
amarderic <amar.deric@webbi.se>
2025-08-18 00:57:24 +0200

    test linkedin

--- routes/web.php
+++ routes/web.php
@@ -182,15 +182,6 @@ Route::middleware(['auth','verified','onboarded', 'paidOrTrial'])->group(functio
     Route::get('/marketing/newsletter', NewsletterCompose::class)->name('marketing.newsletter');
     Route::get('/marketing/mailchimp/history', MailchimpHistory::class)->name('marketing.mailchimp.history');
 
-    Route::get('/auth/facebook/redirect', [SocialAuthController::class, 'facebookRedirect'])->name('auth.facebook.redirect');
-    Route::get('/auth/facebook/callback', [SocialAuthController::class, 'facebookCallback'])->name('auth.facebook.callback');
-
-    Route::get('/auth/linkedin/redirect', [SocialAuthController::class, 'linkedinRedirect'])->name('auth.linkedin.redirect');
-    Route::get('/auth/linkedin/callback', [SocialAuthController::class, 'linkedinCallback'])->name('auth.linkedin.callback');
-
-    Route::get('/auth/instagram/redirect', [SocialAuthController::class, 'instagramRedirect'])->name('auth.instagram.redirect');
-    Route::get('/auth/instagram/callback', [SocialAuthController::class, 'instagramCallback'])->name('auth.instagram.callback');
-
     Route::get('/api/linkedin/suggestions', [LinkedInSuggestionController::class, 'index'])->name('linkedin.suggestions.index');
     Route::post('/api/linkedin/suggestions', [LinkedInSuggestionController::class, 'store'])->name('linkedin.suggestions.store');
     Route::delete('/api/linkedin/suggestions/{suggestion}', [LinkedInSuggestionController::class, 'destroy'])->name('linkedin.suggestions.destroy');
